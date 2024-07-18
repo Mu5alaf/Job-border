918b003414fc77b3af6e7d23202b464df61bc5e6
Mu5alaf <madmedo73@gmail.com>
2024-07-18 10:15:00 +0300

    update model of job

--- job/models.py
+++ job/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
 # Create your models here.
+
+#Job_Type
+JOB_TYPE = (
+    ('Full time','Full Time'),
+    ('Part time','Part Time'),
+)
+class Job(models.Model):
+    title = models.CharField(max_length=100,blank=False,null=False)
+    job_type = models.CharField(max_length=15,choices=JOB_TYPE,blank=False,null=False)
+    description = models.TextField(max_length=999,blank=False,null=False,default='Write a description')
+    created_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1,null=False,blank=False)
+    salary = models.IntegerField(default=0,null=False,blank=False)
+    experiences = models.IntegerField(default=1,null=False,blank=False)
+    
+    def __str__(self):
+        return self.title
