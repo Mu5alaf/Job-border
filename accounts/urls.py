9d8c7e1912b8cfcc0346278745da8c4f24aac775
Mu5alaf <madmedo73@gmail.com>
2024-07-29 06:32:53 +0300

    add signup view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-app_name = 'job'
+app_name = 'accounts'
 urlpatterns = [
     path('signup/',views.signup_view,name='sign_up'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
+from .forms import signup_form
+from django.contrib import messages
 
 # Create your views here.
 def signup_view(request):
-    pass
+    if request.method=='POST':
+        form = signup_form(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(user=username,password=password)
+            login(request,user)
+            messages.success(request,"You have been successfully Signup.")
+            return redirect('accounts/profile')
+        else:
+            messages.error(request,"There was an error during apply")
+            return redirect('accounts:sign_up')
+    else:
+        form = signup_form()
+    return render(request,'registration/signup.html',{'form':form})
