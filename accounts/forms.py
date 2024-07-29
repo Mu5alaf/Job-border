4d121d30ede289f3e5aa08f6ceadf1e15cc52978
Mu5alaf <madmedo73@gmail.com>
2024-07-29 15:59:27 +0300

    adding edit profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,18 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import User
-
+from .models import Profile
 class signup_form(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
         
-    
+class User_Form(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username','first_name','last_name','email']
+        
+class Profile_Form(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['city','phone_number','image']
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login
-from .forms import signup_form
+from .forms import signup_form ,User_Form,Profile_Form
+from .models import Profile
 from django.contrib import messages
 
 # Create your views here.
@@ -21,3 +22,32 @@ def signup_view(request):
     else:
         form = signup_form()
     return render(request,'registration/signup.html',{'form':form})
+
+def Profile_view(request):
+    user = Profile.objects.get(user=request.user)
+    if request.user.is_authenticated:
+        return render(request, "accounts/profile.html", {'user':user})
+    else:
+        return render(request, "registration/login.html")
+
+
+def Profile_edit_view(request):
+    profile = Profile.objects.get(user=request.user)
+    if request.method=='POST':
+        user_form = User_Form(request.POST,instance=request.user)
+        profile_form = Profile_Form(request.POST,request.FILES,instance=profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            my_profile = profile_form.save(commit=False)
+            my_profile.user = request.user
+            my_profile.save()
+            messages.success(request,"You have been successfully Edited.")
+            return redirect('accounts:profile')
+        else:
+            messages.error(request,"There was an error during apply")
+            return redirect('accounts:profile_edit')
+    else:
+        user_form = User_Form(instance=request.user)
+        profile_form = Profile_Form(instance=profile)
+    return render(request,'accounts/edit_profile.html',{'user_form':user_form,'profile_form':profile_form})
+        
