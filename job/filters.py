95e2454ba54df86f2b55e9cecceb803a75dcc157
Mu5alaf <madmedo73@gmail.com>
2024-08-01 10:25:16 +0300

    adding update

---
+++ job/filters.py
@@ -0,0 +1,7 @@
+import django_filters
+from .models import Job
+
+class Job_Filter(django_filters.FilterSet):
+    class Meta:
+        model = Job
+        fields = ['title', 'job_type', 'manufacturer']
--- project/settings.py
+++ project/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'bootstrap4',
     "crispy_forms",
     "crispy_bootstrap4",
+    'django_filters',
 ]
 
 
